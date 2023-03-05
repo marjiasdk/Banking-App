fcf48b9934df96515a62cb50de63dd1fd20f9102
Marjia Siddik <marjia.siddik3@mail.dcu.ie>
2023-03-05 23:58:59 +0000

    Create sample_data.sql

---
+++ SQL/sample_data.sql
@@ -0,0 +1,8 @@
+SELECT * FROM users;
+
+INSERT INTO users (name, card_number, password, balance) VALUES
+('John Smith', '1234567890123', 'password123', 1000.00),
+('Jane Doe', '2345678901234', 'password456', 500.00),
+('Bob Johnson', '3456789012345', 'password789', 250.00),
+('Sarah Lee', '4567890123456', 'passwordabc', 100.00),
+('David Kim', '5678 901234567', 'passworddef', 50.00);
