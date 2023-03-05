008f82d7341d0c24512ec21b833b1b4be4ca59cc
Marjia Siddik <marjia.siddik3@mail.dcu.ie>
2023-03-05 20:23:09 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,10 @@
+-- This creates the database
 CREATE DATABASE banking;
 
+-- This goes into the database so you can use it
 USE banking;
 
+-- Creates a table named "userS", this is where all the data is stored which is used in the program
 CREATE TABLE users (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255),
@@ -10,7 +13,8 @@ CREATE TABLE users (
   balance DECIMAL(10, 2)
 );
 
-INSERT INTO users (card_number, password, balance)
-VALUES 
+-- You can add more data with the following code
+-- (x, y, z) are the different parameters
+INSERT INTO users (card_number, password, balance) VALUES 
   ('1234567890123456', 'password123', 1000.00),
   ('2345678901234567', 'letmein', 5000.00);
