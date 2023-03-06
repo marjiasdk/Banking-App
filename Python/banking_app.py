db5dcbf2559d107c1ac064650813f8e221493a5c
Marjia Siddik <marjia.siddik3@mail.dcu.ie>
2023-03-06 12:50:41 +0000

    Update banking_app.py

--- Python/banking_app.py
+++ Python/banking_app.py
@@ -6,41 +6,36 @@ import mysql.connector
 bank_details = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="isuckcockX1#",
     database="banking"
 )
 
-# create a function to check the login details and display the balance or error message
-def check_login():
-    name = name_entry.get()
-    card_number = card_number_entry.get()
-    password = password_entry.get()
+# create a class to check login details
+class CheckLogin:
+    def __init__(self): # constructor
+        # get the values from the entry boxes
+        self.name = name_entry.get()
+        self.card_number = card_number_entry.get()
+        self.password = password_entry.get()
 
-    # execute a SELECT query to check if the card number and password match the data in the database
-    cursor = bank_details.cursor()
+        cursor = bank_details.cursor()
+        query = "SELECT balance FROM users WHERE name =%s AND card_number=%s AND password=%s"
+        cursor.execute(query, (self.name, self.card_number, self.password))
 
-    # SELECT: used to select data from a database
-    # FROM: specifies the table from which to select data
-    # WHERE: specifies which record(s) to select
-    # %s: a placeholder for the values to be inserted
-    query = "SELECT balance FROM users WHERE name =%s AND card_number=%s AND password=%s"
-    cursor.execute(query, (name, card_number, password)) # execute() executes the query and inserts the values in the placeholders
+        # fetch the result
+        result = cursor.fetchone()
 
-    # fetchone() returns the first row of the result
-    # result is a tuple containing the balance: (balance)
-    result = cursor.fetchone()
-
-    if result is None:
-        error_label.config(text="Invalid name or card number or password") # config() configures the widget
-    else:
-        balance_label.config(text="Your Balance is: €" + str(result[0])) # result[0] returns the balance from the tuple
+        if result is None:
+            error_label.config(text="Invalid name or card number or password")
+        else:
+            balance_label.config(text="Your Balance is: €" + str(result[0]))
 
 # create GUI
 window = Tk()
 window.title("Banking App")
 window.geometry("500x500")
 
-# name label and entry
+# name label and entry  
 name_label = Label(window, text="Name: ")
 name_label.grid(row=0, column=0)
 name_entry = Entry(window)
@@ -58,7 +53,7 @@ password_label.grid(row=2, column=0)
 password_entry = Entry(window, show="*")
 password_entry.grid(row=2, column=1)
 
-button = Button(window, text="Login", command=check_login) # command=check_login: calls the check_login() function when the button is clicked
+button = Button(window, text="Login", command=CheckLogin) # command=check_login: calls the check_login() function when the button is clicked
 button.grid(row=3, column=1)
 
 balance_label = Label(window)
